4c98c48514cc2b3c98512215c55db0ddf558cff8
Juan Carlos <juancarss2002@gmail.com>
2024-03-01 11:52:36 +0000

    added dataset skeleton and removed residual layer

--- framework/toolbox/dataset.py
+++ framework/toolbox/dataset.py
@@ -1,2 +1,31 @@
-class Dataset:
-    pass
+from abc import ABC, abstractmethod
+
+
+class Dataset(ABC):
+    def __init__(self, batch_size: int): #TODO no se qué argumentos hacen falta como tal te lo dejo a tu elección
+        self.seed = "random seed"
+        self.batch_size = batch_size
+
+    @abstractmethod
+    def train(self) -> 'Dataset': #TODO no se si es un dataset lo que devuelve o una lista de batches, la cosa es que después de un train solo pueda obtener los batches
+        pass
+
+    @abstractmethod
+    def evaluation(self) -> 'Dataset':
+        pass
+
+    @abstractmethod
+    def test(self) -> 'Dataset':
+        pass
+
+    @abstractmethod
+    def batches(self) -> 'Dataset':
+        pass
+
+    @abstractmethod
+    def inputs(self): #TODO puede ir fuera en caso de que te crees una clase Batch
+        pass
+
+    @abstractmethod
+    def targets(self):
+        pass
--- implementations/pytorch/architecture/layers/residual.py
+++
@@ -1,7 +0,0 @@
-from framework.architecture.layers.residual import ResidualLayer
-from implementations.pytorch.architecture.layer import PytorchLayer
-
-
-class PytorchResidualLayer(PytorchLayer, ResidualLayer):
-    def __init__(self):
-        super(PytorchResidualLayer, self).__init__()
