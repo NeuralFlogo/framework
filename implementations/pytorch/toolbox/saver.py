e095f82318f90b4bb92efb8060e31ee8481c98cf
Joeel71 <elnanito2002@gmail.com>
2024-04-16 14:40:46 +0100

    saver bug fixed

--- implementations/pytorch/toolbox/saver.py
+++ implementations/pytorch/toolbox/saver.py
@@ -24,10 +24,11 @@ class PytorchModelSaver(ModelSaver):
         self.__checkpoint_counter += 1
 
     def __init_manifest(self, experiment):
-        if not self.__is_dir(self.root + Delimiter + experiment):
-            self.__mkdir(self.root + Delimiter + experiment)
+        if not self.__is_dir(self.__experiment_path(experiment)):
+            self.__create_experiment_folder(experiment)
 
     def __save_weigths(self, path: str, component: PytorchModel | PytorchOptimizer):
+        self.__mkdir(os.path.dirname(path))
         torch.save(component.weights(), path)
 
     def __path_of(self, experiment: str):
@@ -38,10 +39,13 @@ class PytorchModelSaver(ModelSaver):
 
     def __create_experiment_folder(self, experiment):
         self.__mkdir(self.__path_of(experiment))
-        self.__create_manifest(self.__path_of(experiment), experiment)
+        self.__create_manifest(self.__experiment_path(experiment), experiment)
 
     def __mkdir(self, path):
         os.makedirs(path)
 
     def __create_manifest(self, path, experiment):
         pass
+
+    def __experiment_path(self, experiment):
+        return self.root + Delimiter + experiment
