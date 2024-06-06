b28dc0bce3627de4a56cae0fbf8c3c16f1623a05
Joeel71 <elnanito2002@gmail.com>
2024-06-06 18:30:31 +0100

    dataset numeric updated

--- implementations/pytorch/toolbox/data/loaders/numeric.py
+++ implementations/pytorch/toolbox/data/loaders/numeric.py
@@ -1,3 +1,4 @@
+import ast
 import random
 
 import pandas as pd
@@ -6,10 +7,10 @@ from framework.toolbox.data.loader import DatasetLoader
 from implementations.pytorch.toolbox.data.datasets.numeric import PytorchNumericDataset
 
 Delimiter = "delimiter"
-Header = "header"
 Extension = ".csv"
 TargetColumn = "prediction"
 TargetColumnParameter = "target"
+TypeColumn = "list"
 
 
 class PytorchNumericDatasetLoader(DatasetLoader):
@@ -17,7 +18,13 @@ class PytorchNumericDatasetLoader(DatasetLoader):
         return PytorchNumericDataset(batch_size, data, self.__target_column())
 
     def load(self):
-        data = pd.read_csv(self.filename(Extension), delimiter=self.__delimiter(), header=0)
+        if self.__are_list():
+            data = pd.read_csv(self.filename(Extension), delimiter=self.__delimiter(), header=0)
+            for column in data.columns:
+                if self.__is_list_string(data[column][0]):
+                    data[column] = [ast.literal_eval(element) for element in data[column]]
+        else:
+            data = pd.read_csv(self.filename(Extension), delimiter=self.__delimiter(), header=0)
         if self.metadata['task'] == 'classification':
             data[self.__target_column()] = data[self.__target_column()].astype(int)
         return self.__shuffle(data)
@@ -31,3 +38,13 @@ class PytorchNumericDatasetLoader(DatasetLoader):
     def __shuffle(self, dataset):
         random.seed(self.random_state)
         return dataset.sample(frac=1).reset_index(drop=True)
+
+    def __are_list(self):
+        return self.metadata.get(TypeColumn, "numeric") == "list"
+
+    def __is_list_string(self, column):
+        try:
+            result = eval(column)
+            return isinstance(result, list)
+        except:
+            return False
